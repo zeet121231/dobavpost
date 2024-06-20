252795dd0f34396e6e7d21e8c4367f3d5568cb94
Grisha Kamaev <grishakamaev93@gmail.com>
2024-06-20 13:37:55 +0400

    second commit

---
+++ dobav.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 20 2024 г., 12:36
+-- Версия сервера: 5.7.39
+-- Версия PHP: 8.0.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `dobav`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `dobav`
+--
+
+CREATE TABLE `dobav` (
+  `input` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `text_area` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `dobav`
+--
+
+INSERT INTO `dobav` (`input`, `text_area`) VALUES
+('sdfssssssssss', 'ssssssssssssssss');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
